f9abec9df1948b372e4aa9badce0c4c7f5aaf121
Carlie Duperval <carlierichell@gmail.com>
2022-02-23 13:20:36 -0500

    TodaDB File Updated

--- task.js
+++ task.js
@@ -1,3 +1,4 @@
+// import Package Mongo
 import { MongoClient, ObjectId } from "mongodb";
 
 const url = "mongodb://localhost:27017";
@@ -5,7 +6,10 @@ const client = new MongoClient(url);
 
 const dbName = "todo";
 const createTask = async (providedName) => {
+  // connect to server
   await client.connect();
+
+  // connect to data base
   const db = client.db(dbName);
 
   const collection = db.collection("tasks");
@@ -32,15 +36,15 @@ const createMultipleTask = async (taskList) => {
   console.log(result);
 };
 
-//createMultipleTask ([
+// createMultipleTask ([
 //      { name: "Banan", done: false } ,
-//       { name: "Crakers", done: false}, 
-//       { name: "Chocolate", done: false}, 
+//       { name: "Crakers", done: false},
+//       { name: "Chocolate", done: false},
 //       { name: "Papaya", done: false},
-//        { name: "Pear", done: false}, 
+//        { name: "Pear", done: false},
 //        { name: "Cherry", done: false},
-//         { name :"Cheese", done: false}, 
-//         { name : "Sugar", done: false}, 
+//         { name :"Cheese", done: false},
+//         { name : "Sugar", done: false},
 //         { name: "Slat", done: false}
 //     ])
 
@@ -62,28 +66,55 @@ const getAllTask = async () => {
 
 // Update
 
-const updateTask = async (id) => {
-  // connect to the server
-  await client.connect();
+// //const updateTask = async (id) => {
+//   // connect to the server
+//   await client.connect();
 
-  const db = client.db(dbName);
-  const collection = db.collection("tasks");
+//   const db = client.db(dbName);
+//   const collection = db.collection("tasks");
 
-  const objId = new ObjectId(id);
-  const updateTask = await collection.updateOne(
-    { _id: objId },
-    { $set: { done: true } }
-  );
+//   const objId = new ObjectId(id);
+//   const updateTask = await collection.updateOne(
+//     { _id: objId },
+//     { $set: { done: true } }
+//   );
 
-  console.log(updateTask);
-  return updateTask;
-};
+//   console.log(updateTask);
+//   return updateTask;
+// };
 //updateTask("621275f53d3b3d7cd47bf34d")
 
+
+// Declare function
+const updateTaskName = async (id, newName) => {
+
+    // connect to the server 
+    await client.connect();
+
+    // connect to the data base
+
+    const db = client.db(dbName);
+    const collection = db.collection('tasks');
+    const objId = new ObjectId(id);
+    const updateNewTask = await collection.updateOne(
+
+        {_id: objId },
+        { $set : { name: newName }}
+        
+        );
+
+        console.log(updateNewTask)
+        return updateNewTask
+    }
+
+updateTaskName("6215167817148bc3d4ed346f", 'Banana')
+
+
+
 //  Delete document
-// declare fubction
+// declare function
 const deleteTask = async (Id) => {
-    // connection to server
+  // connection to server
   await client.connect();
   const db = client.db(dbName);
   const collection = db.collection("tasks");
@@ -99,4 +130,5 @@ const deleteTask = async (Id) => {
 //deleteTask("621275ad02bdeedadc8e4d39")
 //deleteTask("621275f53d3b3d7cd47bf34d")
 //deleteTask("62127649130f11b3fd7e1f92")
-deleteTask("6214e93fea5558617bf02813")
+//deleteTask("6214e93fea5558617bf02813")
+//deleteTask("6214fae0a9960809421ca778")
